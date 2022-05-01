56d67c74719edacbe411884ad4db4fdd853ea5e5
G-Lake <86152185+G-Lake@users.noreply.github.com>
2022-05-01 23:40:19 +0900

    Add files via upload

---
+++ 주사위세개_2480.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class 주사위세개_2480 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+        
+        if(a == b && b == c)
+            System.out.print(10000 + a*1000);
+        else if(a == b || a == c)
+            System.out.print(1000 + a*100);
+        else if(b == c)
+            System.out.print(1000 + b*100);
+        else
+            System.out.print((Math.max(Math.max(a, b), c)*100));
+    }
+}
