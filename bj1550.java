57c2a796d3a98700a74d63550ecfa6b4f2146418
이원진 <103166677+Wonjin0138@users.noreply.github.com>
2022-05-27 23:53:00 +0900

    Add files via upload

---
+++ bj1550.java
@@ -0,0 +1,14 @@
+package hello1;
+import java.math.*;
+import java.util.*;
+public class bj1550 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		int b = Integer.parseInt(a,16);
+		System.out.println(b);
+
+	}
+
+}
