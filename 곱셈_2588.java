40ea13a7497cce50095ddcea5cda8d4c38555238
G-Lake <86152185+G-Lake@users.noreply.github.com>
2022-05-29 12:59:33 +0900

    Add files via upload

---
+++ 곱셈_2588.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class °ö¼À_2588{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        
+        int c = a * (b%10);
+        int d = a * ((b/10)%10);
+        int e = a * (b/100);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(c + d*10 + e*100);
+    }
+}
